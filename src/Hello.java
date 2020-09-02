d477aa5517c2b751ace4a23f37f9ea697c4b4472
dhoin9 <dhoin9@op.pl>
2020-09-02 10:50:05 +0200

    test3

--- src/Hello.java
+++ src/Hello.java
@@ -3,7 +3,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello Czarek");
 
-        int myFirstNumber = 5+10;
+        int myFirstNumber = 5+10+14;
         System.out.println(myFirstNumber);
     }
 }
