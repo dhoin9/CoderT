fc0c2d865cdccda7b999406776582579a6da6c21
dhoin9 <dhoin9@op.pl>
2020-09-02 09:14:23 +0200

    test2

---
+++ src/Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Czarek");
+
+        int myFirstNumber = 5+10;
+        System.out.println(myFirstNumber);
+    }
+}
